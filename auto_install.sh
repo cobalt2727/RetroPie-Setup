377b476ab95554070545804fd8a4dc6147ec4f0e
Garrett <gmesmer@comcast.net>
2020-08-31 20:14:15 -0400

    fix retropie install and check for sdl2 version before install
    automatic install actually works now using auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -6,5 +6,14 @@ git clone --depth=1 https://github.com/theofficialgman/RetroPie-Setup.git
 
 #auto install retropie with most important emulators (which don't take up much space)
 cd RetroPie-Setup
-sudo ./retropie_packages.sh setup basic_install_setup
+sudo ./retropie_packages.sh setup basic_install
+if dpkg -s libsdl2-dev | grep -q "2.0.10+5"; then
+echo ""
+echo "Already Installed Newest SDL2 Version"
+sleep 3
+else
 sudo ./retropie_packages.sh sdl2
+echo ""
+echo "Successfully Installed Newest SDL2 Version"
+sleep 3
+fi
