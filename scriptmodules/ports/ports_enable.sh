14011e5727726fa9ca22be61806fcf6af49e6f88
Garrett <gmesmer@comcast.net>
2020-09-07 17:59:58 -0400

    enable ports script

---
+++ scriptmodules/ports/ports_enable.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="ports-enable"
+rp_module_desc="Simple Script to Enable the Ports Screen"
+rp_module_section="exp"
+
+function configure_ports-enable() {
+    addSystem "ports"
+}
